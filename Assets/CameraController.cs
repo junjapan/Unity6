b0e3cc9e518a32abefc579d7b236bcba966f7c6b
jun <watanabejapan2trap@gmail.com>
2022-02-17 14:58:51 +0900

    terrain作成

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public float pointX = -3.0f;
+    public float pointY = 1.0f;
+    public float rotX;
+    public float rotY;
+
+    void LateUpdate() {
+        transform.position = player.position + (player.forward * pointX) + (player.up * pointY);
+        Vector3 dir = player.forward;
+        dir.x += rotX;
+        dir.y += rotY;
+        transform.rotation = Quaternion.LookRotation(dir);
+    }
+}
